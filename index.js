18e683ee647c7d077379777ca7b5ef9d3644159b
Al Ekram Hossain Abir <alabir65@gmail.com>
2025-10-03 11:29:26 +0600

    redirecting url

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const {ConnectToMongoDb} = require('./connect');
+const { ConnectToMongoDb } = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 const PORT = process.env.PORT || 3000;
 
-ConnectToMongoDb({ url: 'mongodb://127.0.0.1:27017/urlShortner' })
-    .then(() => console.log("Database connected"))
-    .catch((err) => {
-        console.error("Failed to connect to the database:", err);
-        process.exit(1); // Exit the process if the database connection fails
-    });
+ConnectToMongoDb({ url: "mongodb://127.0.0.1:27017/urlShortner" })
+  .then(() => console.log("Database connected"))
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1); // Exit the process if the database connection fails
+  });
 
 app.use(express.json());
 
 app.use("/url", urlRoute);
 
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const result = await URL.findOneAndUpdate(
+    { shortId: shortId },
+    {
+      $push: {
+        visitedHistory: {
+          timestamps: Date.now(),
+        },
+      },
+    }
+  );
+  console.log(result.redirectUrl);
+
+  res.redirect(result.redirectUrl);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
